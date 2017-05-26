8355cf78d49ff2b7e86b159f8914784efec90ebc
teklynk <ryanjones153@gmail.com>
2017-05-26 12:30:21 -0400

    added config

--- config/config.php
+++ config/config.php
@@ -1,5 +1,11 @@
 <?php
+//Get Sub-folder name
+if (basename(dirname($_SERVER['PHP_SELF']))) {
+    $subDirectory = "/".basename(dirname($_SERVER['PHP_SELF']));
+} else {
+    $subDirectory = "";
+}
+
 $IPrange = "555.555.";
 $customerLinkStr = "https://intranet.tlcdelivers.com/TLCWebLSN/customer.asp?Cust_ID=";
 $buildURLStr = "";
-?>
