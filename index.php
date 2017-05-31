54f28bc7c506d80b9d86c788e32526e07430a753
teklynk <ryanjones153@gmail.com>
2017-05-31 16:56:55 -0400

    refactored modals, add,edit,deletes work with db

--- index.php
+++ index.php
@@ -15,44 +15,74 @@ include_once('includes/header.inc.php');
 <div class="container">
     <div class="card">
         <div class="card-body">
-            <?php
-            if ($_GET['form'] == 'add' || $_GET['form'] == 'edit') {
 
-                $sqlSite = mysqli_query($db_conn, "SELECT * FROM sites WHERE id=" . $_GET['id'] . " ");
-                $rowSite = mysqli_fetch_array($sqlSite);
+                <!--modal window-->
+                <div id="myModal" class="modal fade" role="dialog" data-keyboard="false" data-backdrop="static">
+                    <div class="modal-dialog modal-sm">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <a type="button" class="close" data-dismiss="modal">
+                                    <i class="fa fa-times"></i>
+                                </a>
+                                <h4 class="modal-title">&nbsp;</h4>
+                            </div>
+                            <?php
+                            $sqlSite = mysqli_query($db_conn, "SELECT * FROM sites WHERE id=" . $_GET['id'] . " ");
+                            $rowSite = mysqli_fetch_array($sqlSite);
 
-                if (!empty($_POST)) {
+                            if (!empty($_POST)) {
+                                if (!empty($_POST['loc_id'])) {
+                                    //update data on submit
+                                    $siteUpdate = "UPDATE sites SET customerid='" . safeCleanStr($_POST['cust_number']) . "', name='" . safeCleanStr($_POST['site_name']) . "', date='" . date("Y-m-d H:i:s") . "' WHERE id=" . $_POST['loc_id'] . " ";
+                                    mysqli_query($db_conn, $siteUpdate);
+                                } elseif (!empty($_POST['delete_id'])) {
+                                    //delete site
+                                    $siteDelete = "DELETE FROM sites WHERE id=" . $_POST['delete_id'] . " ";
+                                    mysqli_query($db_conn, $siteDelete);
+                                } else {
+                                    //insert data on submit
+                                    $siteInsert = "INSERT INTO sites (customerid, name, version, date) VALUES ('" . safeCleanStr($_POST['cust_number']) . "', '" . safeCleanStr($_POST['site_name']) . "', '" . safeCleanStr('') . "', '" . date("Y-m-d H:i:s") . "')";
+                                    mysqli_query($db_conn, $siteInsert);
+                                }
+                            }
 
-                    if ($_POST['loc_id'] <> ''){
-                        //update data on submit
-                        $siteUpdate = "UPDATE sites SET customerid='" . safeCleanStr($_POST['cust_number']) . "', name='" . safeCleanStr($_POST['site_name']) . "', date='" . date("Y-m-d H:i:s") . "' WHERE id=" . $_POST['loc_id'] . " ";
-                        mysqli_query($db_conn, $siteUpdate);
-                    } else {
-                        //insert data on submit
-                        $siteInsert = "INSERT INTO sites (customerid, name, version, date) VALUES ('" . safeCleanStr($_POST['cust_number']) . "', '" . safeCleanStr($_POST['site_name']) . "', '" . safeCleanStr('') . "', '" . date("Y-m-d H:i:s") . "')";
-                        mysqli_query($db_conn, $siteInsert);
-                    }
+                            if ($_GET['form'] == 'delete'){
+                                $disableInputs = 'disabled';
+                            } else {
+                                $disableInputs = '';
+                            }
+                            ?>
+                            <div class="modal-body">
+                                <form name="addeditform" id="addeditform" method="post" action="index.php">
+                                    <h1 class="page-header"><?php echo ucwords($_GET['form']); ?> Site</h1>
+                                    <div class="form-group">
+                                        <label>Customer Number</label>
+                                        <input class="form-control" <?php echo $disableInputs; ?> value="<?php echo $rowSite['customerid']; ?>" maxlength="100" placeholder="8675309" id="cust_number" name="cust_number" type="text" autocomplete="off" autofocus required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Site Name</label>
+                                        <input class="form-control" <?php echo $disableInputs; ?> value="<?php echo $rowSite['name']; ?>" maxlength="100" placeholder="handleypl" id="site_name" name="site_name" type="text" autocomplete="off" required>
+                                    </div>
 
-                }
-                ?>
-                <form id="addeditform" method="post" action="">
-                    <h1 class='page-header'><?php echo ucwords($_GET['form']); ?> Site</h1>
-                    <div class="form-group">
-                        <label>Customer Number</label>
-                        <input class="form-control" value="<?php echo $rowSite['customerid']; ?>" maxlength="100" placeholder="8675309" id="cust_number" name="cust_number" type="text" autocomplete="off" autofocus required>
-                    </div>
-                    <div class="form-group">
-                        <label>Site Name</label>
-                        <input class="form-control" value="<?php echo $rowSite['name']; ?>" maxlength="100" placeholder="handleypl" id="site_name" name="site_name" type="text" autocomplete="off" required>
-                    </div>
-                    <input type="hidden" name="loc_id" value="<?php echo $rowSite['id']; ?>"/>
-                    <button class="btn btn-lg btn-primary btn-block" type="submit" onclick="javascript:window.location.reload()" id="addeditsubmit" name="addeditsubmit">Save</button>
-                </form>
-                <?php
-            } else {
-                ?>
+                                    <?php
+                                    if ($_GET['form'] == 'delete'){
+                                        echo "<input type='hidden' id='delete_id' name='delete_id' value='".$_GET['id']."'/>";
+                                        echo "<button class='btn btn-danger' type='submit' id='deletesubmit' name='deletesubmit'><i class='fa fa-trash'></i> Delete</button>";
+                                    } else {
+                                        echo  "<input type='hidden' id='loc_id' name='loc_id' value='".$_GET['id']."'/>";
+                                        echo "<button class='btn btn-primary' type='submit' id='addeditsubmit' name='addeditsubmit'><i class='fa fa-save'></i> Save</button>";
+                                    }
+                                    ?>
+                                    <button class="btn btn-default" id="cancel" data-dismiss="modal">Cancel</button>
+                                </form>
+                            </div>
+                            <div class="modal-footer">&nbsp;</div>
+                        </div><!-- /.modal-content -->
+                    </div><!-- /.modal-dialog -->
+                </div><!-- /.modal -->
+
                 <h1 class="page-header">Active Sites</h1>
-                <button type="button" data-toggle="tooltip" title="Add a New Site" class="btn btn-primary" onclick="showMyModal('New Site', 'index.php?modal=true&form=add')"><i class="fa fa-fw fa-plus"></i> Add a New Site</button>
+                <button type="button" title="Add a New Site" class="btn btn-primary" id="add" onClick="window.location='index.php?add=true&form=add&id=';"><i class="fa fa-fw fa-plus"></i> Add a New Site</button>
                 <br/><br/>
                 <table class="table table-bordered table-hover table-striped table-responsive dataTable" id="dataTable">
                     <thead>
@@ -69,19 +99,19 @@ include_once('includes/header.inc.php');
 
                         <?php
 
-                        $sqlSite = mysqli_query($db_conn, "SELECT * FROM sites");
-                        while ($rowSite = mysqli_fetch_array($sqlSite)) {
+                        $sqlSiteList = mysqli_query($db_conn, "SELECT * FROM sites");
+                        while ($rowSiteList = mysqli_fetch_array($sqlSiteList)) {
 
                             ?>
                             <tr>
-                                <td><a href="https://intranet.tlcdelivers.com/TLCWebLSN/customer.asp?Cust_ID=<?php echo $rowSite['customerid']; ?>" target="_blank"><?php echo $rowSite['customerid']; ?></a></td>
-                                <td><?php echo $rowSite['name']; ?></td>
+                                <td><a href="https://intranet.tlcdelivers.com/TLCWebLSN/customer.asp?Cust_ID=<?php echo $rowSiteList['customerid']; ?>" target="_blank"><?php echo $rowSiteList['customerid']; ?></a></td>
+                                <td><?php echo $rowSiteList['name']; ?></td>
                                 <td><a href="#" target="_blank">Admin Link</a></td>
-                                <td><?php echo $rowSite['version']; ?></td>
-                                <td><?php echo $rowSite['date']; ?></td>
+                                <td><?php echo $rowSiteList['version']; ?></td>
+                                <td><?php echo $rowSiteList['date']; ?></td>
                                 <td class="col-xs-2">
-                                    <button type="button" data-toggle="tooltip" title="Edit" class="btn btn-primary" onclick="showMyModal('Edit Site', 'index.php?modal=true&form=edit&id=<?php echo $rowSite['id']; ?>')"><i class='fa fa-fw fa-edit'></i></button>
-                                    <button type="button" data-toggle="tooltip" title="Delete" class="btn btn-danger" onclick="window.location.href='index.php?deletetitle=<?php echo $rowSite['name']; ?>&id=<?php echo $rowSite['id']; ?>'"><i class='fa fa-fw fa-trash'></i></button>
+                                    <button type="button" title="Edit" class="btn btn-primary" id="edit" onClick="window.location='index.php?edit=true&form=edit&id=<?php echo $rowSiteList['id']; ?>';"><i class='fa fa-fw fa-edit'></i></button>
+                                    <button type="button" title="Delete" class="btn btn-danger" id="delete" onClick="window.location='index.php?delete=true&form=delete&id=<?php echo $rowSiteList['id']; ?>';"><i class='fa fa-fw fa-trash'></i></button>
                                 </td>
                             </tr>
                             <?php
@@ -92,31 +122,14 @@ include_once('includes/header.inc.php');
 
                     </tbody>
                 </table>
-                <?php
-            }
-            ?>
         </div>
     </div>
 </div>
-
-<!--modal window-->
-<div class="modal fade" id="webslideDialog" class="modal hide fade" data-keyboard="false" data-backdrop="static">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <a type="button" class="close" data-dismiss="modal">
-                    <i class="fa fa-times"></i>
-                </a>
-                <h4 class="modal-title">&nbsp;</h4>
-            </div>
-            <div class="modal-body">
-                <iframe id="myModalFile" src="" frameborder="0"></iframe>
-            </div>
-            <div class="modal-footer">&nbsp;</div>
-        </div><!-- /.modal-content -->
-    </div><!-- /.modal-dialog -->
-</div><!-- /.modal -->
-
+<style>
+    .modal-sm {
+        width: 25%;
+    }
+</style>
 <script type="text/javascript">
     $(document).ready(function () {
         $('#dataTable').dataTable({
@@ -127,16 +140,19 @@ include_once('includes/header.inc.php');
                 "orderable": false
             }]
         });
-        $('#addeditsubmit').click(function() {
-            setTimeout(function(){
-                window.parent.location.reload();
-            }, 500);
-        });
-        $('#webslideDialog').on('hidden.bs.modal', function() {
+
+        $('#myModal').on('hidden.bs.modal', function() {
             setTimeout(function(){
                 window.location.href='index.php';
-            }, 500);
+            }, 250);
         });
+
+        var url = window.location.href;
+        if (url.indexOf('?edit=true') != -1 || url.indexOf('?delete=true') != -1 || url.indexOf('?add=true') != -1 ) {
+            setTimeout(function(){
+                $('#myModal').modal('show');
+            }, 250);
+        }
     });
 </script>
 <?php
