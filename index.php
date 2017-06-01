26cba5d14efa599d0f4991dd98292c07a55a9893
teklynk <ryanjones153@gmail.com>
2017-06-01 16:56:40 -0400

    added rename

--- index.php
+++ index.php
@@ -47,7 +47,7 @@ include_once('includes/header.inc.php');
                         $siteUpdate = "UPDATE sites SET customerid='" . $custNumber . "', name='" . $siteName . "', sid='" . $custSid . "', version='', date='" . date("Y-m-d H:i:s") . "' WHERE id=" . $_POST['loc_id'] . " ";
                         mysqli_query($db_conn, $siteUpdate);
 
-                        renameWithNestedMkdir($ysmSitesDir . "/" . $rowSiteName, $ysmSitesDir . "/" . $siteName) OR die('Could not create directory');
+                        renameWithNestedMkdir($ysmSitesDir . "/" . $rowSiteName, $ysmSitesDir . "/" . $siteName) OR die('Could not create ' . $rowSiteName . ' or ' . $siteName);
                     }
 
                 } elseif (!empty($_POST['delete_id'])) {
